4e4609edbc1d97a782a730482fd83277735ea1a0
Christine Kim <christine@Christines-MacBook-Pro-2.local>
2019-02-09 18:33:34 -0600

    section 2 lecture 35

---
+++ src/app/server/server.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { ServerComponent } from './server.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        ServerComponent,
+    ],
+    exports: [
+        ServerComponent,
+    ]
+})
+export class ServerModule {
+
+}
